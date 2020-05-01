f951656b27a809ac2e8b421f74bc500dc7fb7160
纬林 许 <xuweilin2014@outlook.com>
2020-05-02 05:53:43 +0800

    增加了reference自定义标签的注释

--- src/main/java/com/newlandframework/rpc/spring/NettyRpcReference.java
+++ src/main/java/com/newlandframework/rpc/spring/NettyRpcReference.java
@@ -5,40 +5,17 @@ import com.newlandframework.rpc.event.ClientStopEvent;
 import com.newlandframework.rpc.event.ClientStopEventListener;
 import com.newlandframework.rpc.netty.MessageSendExecutor;
 import com.newlandframework.rpc.serialize.RpcSerializeProtocol;
+import lombok.Data;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.InitializingBean;
 
-
+@Data
 public class NettyRpcReference implements FactoryBean, InitializingBean, DisposableBean {
     private String interfaceName;
     private String ipAddr;
     private String protocol;
     private EventBus eventBus = new EventBus();
-
-    public String getIpAddr() {
-        return ipAddr;
-    }
-
-    public void setIpAddr(String ipAddr) {
-        this.ipAddr = ipAddr;
-    }
-
-    public String getInterfaceName() {
-        return interfaceName;
-    }
-
-    public void setInterfaceName(String interfaceName) {
-        this.interfaceName = interfaceName;
-    }
-
-    public String getProtocol() {
-        return protocol;
-    }
-
-    public void setProtocol(String protocol) {
-        this.protocol = protocol;
-    }
 
     @Override
     public void destroy() throws Exception {
@@ -47,6 +24,12 @@ public class NettyRpcReference implements FactoryBean, InitializingBean, Disposa
 
     @Override
     public void afterPropertiesSet() throws Exception {
+        // 每一次客户端向服务器端建立一个连接时，都会发起一个长连接，比如调用RPC服务器端的addCalculate和multiCalculate方法，
+        // 就会向RPC服务器发起两次连接。不过这样会有一个问题，假设在XML文件中，指定add方法向A服务器发起请求，multi方法向B服务器发起请求，
+        // 接着调用下面这行代码setRpcServerLoader，最终会调用到MessageSendInitializeTask中的call方法，分别向服务器A、B建立长连接。
+        // 但是由于RpcServerLoader是单例的，因此其中所包含的MessageSendHandler是唯一的。因此，最后设置的MessageSendHandler就是与A、B两台服务器
+        // 连接中的一个handler。因此，发送的add方法请求和call方法请求都会通过这个handler发往同一台服务器。但是这个项目设计的只支持
+        // 一个客户端与一个服务器端的通信。
         MessageSendExecutor.getInstance().setRpcServerLoader(ipAddr, RpcSerializeProtocol.valueOf(protocol));
         ClientStopEventListener listener = new ClientStopEventListener();
         eventBus.register(listener);
@@ -54,6 +37,8 @@ public class NettyRpcReference implements FactoryBean, InitializingBean, Disposa
 
     @Override
     public Object getObject() throws Exception {
+        // 返回一个实现了interfaceName的代理对象，调用代理对象的方法时，真正调用的是实现了InvocationHandler的
+        // MessageSendProxy对象的invoke方法
         return MessageSendExecutor.getInstance().execute(getObjectType());
     }
 
