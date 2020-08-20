3d8aa059b518f021aeac65e7280d7c0e647203f2
tom_glb <xuweilin2014@outlook.com>
2020-08-20 17:30:04 +0800

    动态目录增加属性

--- src/main/java/com/xu/rpc/cluster/directory/RegistryDirectory.java
+++ src/main/java/com/xu/rpc/cluster/directory/RegistryDirectory.java
@@ -2,20 +2,32 @@ package com.xu.rpc.cluster.directory;
 
 import com.xu.rpc.exception.RpcException;
 import com.xu.rpc.protocol.Invoker;
+import com.xu.rpc.protocol.Protocol;
 import com.xu.rpc.registry.NotifyListener;
 import com.xu.rpc.registry.Registry;
 import com.xu.rpc.util.URL;
+import io.netty.util.internal.ConcurrentSet;
 import org.apache.log4j.Logger;
+import sun.dc.pr.PRError;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class RegistryDirectory extends AbstractDirectory implements NotifyListener {
 
     private static final Logger logger = Logger.getLogger(RegistryDirectory.class);
 
-    private List<Invoker> invokers;
-
     private Registry registry;
+
+    private Protocol protocol;
+
+    private volatile Map<String, List<Invoker>> methodToInvokers = new ConcurrentHashMap<>();
+
+    private volatile Set<URL> cachedInvokerUrls = new ConcurrentSet<>();
+
+    private volatile Map<String, Invoker> urlToInvokers = new ConcurrentHashMap<>();
 
     public RegistryDirectory(Class<?> type, URL url, Registry registry){
         super(url);
