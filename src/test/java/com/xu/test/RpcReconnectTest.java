603adb2b9c8f8b10856f3771f74d88aa800481fc
纬林 许 <xuweilin2014@outlook.com>
2020-09-20 20:59:39 +0800

    增加了断线重连的测试代码

--- src/test/java/com/xu/test/RpcReconnectTest.java
+++ src/test/java/com/xu/test/RpcReconnectTest.java
@@ -1,12 +1,15 @@
 package com.xu.test;
 
+import com.xu.rpc.core.RpcContext;
 import com.xu.rpc.services.PersonManage;
 import com.xu.rpc.services.pojo.Person;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.concurrent.ExecutionException;
+
 public class RpcReconnectTest {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:rpc-invoke-config-client.xml");
 
         PersonManage manage = (PersonManage) context.getBean("personManage");
@@ -15,10 +18,15 @@ public class RpcReconnectTest {
         p.setId(20150811);
         p.setName("XiaoHaoBaby");
         p.setAge(1);
+        long sleepTime = 60000 * 5;
+        long start = System.currentTimeMillis();
 
-        while (true){
+        while (System.currentTimeMillis() - start <= sleepTime){
             Thread.sleep(1000);
         }
+
+        manage.save(p);
+        System.out.println(RpcContext.getContext().getFuture().get());
     }
 
 }
