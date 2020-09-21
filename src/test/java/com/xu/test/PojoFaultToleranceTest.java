3477a36d5a3d756000235931cdf7e818541e845f
纬林 许 <xuweilin2014@outlook.com>
2020-09-21 22:49:27 +0800

    增加了集群容错的测试代码

---
+++ src/test/java/com/xu/test/PojoFaultToleranceTest.java
@@ -0,0 +1,38 @@
+package com.xu.test;
+
+import com.xu.rpc.core.RpcContext;
+import com.xu.rpc.services.PersonManage;
+import com.xu.rpc.services.pojo.Person;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+public class PojoFaultToleranceTest {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException, IOException {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:rpc-invoke-config-client.xml");
+
+        PersonManage manage = (PersonManage) context.getBean("personManage");
+
+        Person p = new Person();
+        p.setId(20150811);
+        p.setName("XiaoHaoBaby");
+        p.setAge(1);
+
+        //Thread.sleep(20000);
+        int counter = 1;
+        try {
+            while (counter <= 5) {
+                System.out.println("this is the " + counter + " invoke.");
+                System.out.println(manage.save(p));
+                counter++;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.in.read();
+    }
+
+}
