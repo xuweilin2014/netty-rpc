c731b8dde921c85790dc43f6e5ab77e9378067b8
纬林 许 <xuweilin2014@outlook.com>
2020-09-25 23:13:02 +0800

    修复了负载均衡的代码

--- src/main/java/com/xu/rpc/cluster/loadbalance/AbstractLoadBalancer.java
+++ src/main/java/com/xu/rpc/cluster/loadbalance/AbstractLoadBalancer.java
@@ -7,8 +7,19 @@ import com.xu.rpc.protocol.Invoker;
 import com.xu.rpc.commons.URL;
 
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public abstract class AbstractLoadBalancer implements LoadBalancer {
+
+    private static Map<String, Integer> map = new ConcurrentHashMap<>();
+
+    static {
+        map.put("169.254.207.250:10880", 0);
+        map.put("169.254.207.250:10881", 0);
+        map.put("169.254.207.250:10882", 0);
+        map.put("169.254.207.250:10883", 0);
+    }
 
     @Override
     public Invoker select(RpcInvocation invocation, List<Invoker> invokers, URL url) throws RpcException {
@@ -18,7 +29,14 @@ public abstract class AbstractLoadBalancer implements LoadBalancer {
         if (invokers.size() == 1)
             return invokers.get(0);
 
-        return doSelect(invocation, invokers, url);
+        Invoker invoker = doSelect(invocation, invokers, url);
+        System.out.println("choose invoker for url " + invoker.getUrl());
+
+        Integer times = map.get(invoker.getUrl().getAddress());
+        map.put(invoker.getUrl().getAddress(), times + 1);
+        System.out.println(map);
+
+        return invoker;
     }
 
     public abstract Invoker doSelect(RpcInvocation invocation, List<Invoker> invokers, URL url) throws RpcException;
--- src/main/java/com/xu/rpc/cluster/loadbalance/ConsistentHashLoadBalancer.java
+++ src/main/java/com/xu/rpc/cluster/loadbalance/ConsistentHashLoadBalancer.java
@@ -1,6 +1,7 @@
 package com.xu.rpc.cluster.loadbalance;
 
 import com.xu.rpc.commons.URL;
+import com.xu.rpc.core.RpcConfig;
 import com.xu.rpc.core.RpcInvocation;
 import com.xu.rpc.exception.RpcException;
 import com.xu.rpc.protocol.Invoker;
@@ -19,8 +20,8 @@ public class ConsistentHashLoadBalancer extends AbstractLoadBalancer{
 
     @Override
     public Invoker doSelect(RpcInvocation invocation, List<Invoker> invokers, URL url) throws RpcException {
-        // key的值一般为 服务全类名.方法名，比如：com.dubbo.simple.common.DemoService.sayHello
-        String key = invokers.get(0).getUrl().getServiceName() + "." + invocation.getMethodName();
+        // key的值一般为 服务全类名#方法名，比如：com.dubbo.simple.common.DemoService.sayHello
+        String key = invokers.get(0).getUrl().getServiceName() + RpcConfig.HEX_SEPARATOR + invocation.getMethodName();
 
         // 当满足两个条件之一时，会创建或者说重新创建 selector
         // 1.与 key 相对应的 selector 还没有被创建过，selector == null
@@ -54,6 +55,7 @@ public class ConsistentHashLoadBalancer extends AbstractLoadBalancer{
             for (Invoker invoker : invokers) {
                 for (int i = 0; i < REPLICA_NUMBER; i++) {
                     String address = invoker.getUrl().getAddress();
+                    // 在生成虚拟节点的时候，使用的 hash 字符串为：ip:port + 节点序号
                     long key = FNVHash(address + i);
                     virtualNodes.put(key, invoker);
                 }
