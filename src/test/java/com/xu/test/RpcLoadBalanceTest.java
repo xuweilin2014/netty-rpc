f357eea7583e4432b6a5b2c4ae4d8ee4818caf3d
纬林 许 <xuweilin2014@outlook.com>
2020-09-25 23:14:19 +0800

    增加了负载均衡的测试代码

---
+++ src/test/java/com/xu/test/RpcLoadBalanceTest.java
@@ -0,0 +1,29 @@
+package com.xu.test;
+
+import com.xu.rpc.services.PersonManage;
+import com.xu.rpc.services.pojo.Person;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Random;
+
+public class RpcLoadBalanceTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:rpc-invoke-config-client.xml");
+
+        PersonManage manage = (PersonManage) context.getBean("personManage");
+
+        Person p = new Person();
+        p.setId(20150811);
+        p.setName("XiaoHaoBaby");
+        p.setAge(1);
+
+        int counter = 1;
+        while (counter <= 10){
+            Thread.sleep(new Random().nextInt(2000));
+            manage.save(p);
+            counter++;
+        }
+    }
+
+}
